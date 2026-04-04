2c344e50cc4904a3e0ece2cd9bdfbf34668feeba
camtasiabox <camtasiabox@gmail.com>
2026-04-04 23:22:38 +0300

    Add files via upload

---
+++ colorconv/clut_interpolation.js
@@ -0,0 +1,72 @@
+// JavaScript Document created 2020_04_8
+// Encoding: UTF-8
+// charset=UTF-8
+
+A2B0_f39_CLUT_C_Int = [];
+A2B0_f39_CLUT_M_Int = [];
+A2B0_f39_CLUT_Y_Int = [];
+A2B0_f39_CLUT_K_Int = [];
+B2A0_f39_CLUT_C_Output_Int = [];
+B2A0_f39_CLUT_M_Output_Int = [];
+B2A0_f39_CLUT_Y_Output_Int = [];
+B2A0_f39_CLUT_K_Output_Int = [];
+
+B2A0_f39_CLUT_L_Input_Int = [];
+A2B0_f39_CLUT_Input_Int = [];
+
+for (var i=0; i<100.01; i+=0.05) { // 2000 значений до 5 сотых 2001 = 100.05
+	B2A0_f39_CLUT_L_Input_Int.push(parseFloat(i.toFixed(2)));
+	A2B0_f39_CLUT_Input_Int.push(parseFloat(i.toFixed(2)));
+}
+
+for (var j=0; j<4; j++){ // важно 4
+B2A0_f39_CLUT_C_Output_Int[j] = cubicspline(B2A0_f39_CLUT_L_Input[0],B2A0_f39_CLUT_C_Output[0],B2A0_f39_CLUT_L_Input_Int);
+B2A0_f39_CLUT_M_Output_Int[j] = cubicspline(B2A0_f39_CLUT_L_Input[0],B2A0_f39_CLUT_M_Output[0],B2A0_f39_CLUT_L_Input_Int);
+B2A0_f39_CLUT_Y_Output_Int[j] = cubicspline(B2A0_f39_CLUT_L_Input[0],B2A0_f39_CLUT_Y_Output[0],B2A0_f39_CLUT_L_Input_Int);
+B2A0_f39_CLUT_K_Output_Int[j] = cubicspline(B2A0_f39_CLUT_L_Input[0],B2A0_f39_CLUT_K_Output[0],B2A0_f39_CLUT_L_Input_Int);
+} // and < 5
+B2A0_f39_CLUT_C_Output_Int[4] = cubicspline(B2A0_f39_CLUT_L_Input[0],B2A0_f39_CLUT_C_Output[4],B2A0_f39_CLUT_L_Input_Int);
+B2A0_f39_CLUT_M_Output_Int[4] = cubicspline(B2A0_f39_CLUT_L_Input[0],B2A0_f39_CLUT_M_Output[4],B2A0_f39_CLUT_L_Input_Int);
+B2A0_f39_CLUT_Y_Output_Int[4] = cubicspline(B2A0_f39_CLUT_L_Input[0],B2A0_f39_CLUT_Y_Output[4],B2A0_f39_CLUT_L_Input_Int);
+B2A0_f39_CLUT_K_Output_Int[4] = cubicspline(B2A0_f39_CLUT_L_Input[0],B2A0_f39_CLUT_K_Output[4],B2A0_f39_CLUT_L_Input_Int);
+
+for (var j=0; j<5; j++){
+A2B0_f39_CLUT_C_Int[j] = cubicspline(A2B0_f39_CLUT_Input[0],A2B0_f39_CLUT_C[j],A2B0_f39_CLUT_Input_Int); 
+A2B0_f39_CLUT_M_Int[j] = cubicspline(A2B0_f39_CLUT_Input[0],A2B0_f39_CLUT_M[j],A2B0_f39_CLUT_Input_Int);
+A2B0_f39_CLUT_Y_Int[j] = cubicspline(A2B0_f39_CLUT_Input[0],A2B0_f39_CLUT_Y[j],A2B0_f39_CLUT_Input_Int);
+A2B0_f39_CLUT_K_Int[j] = cubicspline(A2B0_f39_CLUT_Input[0],A2B0_f39_CLUT_K[j],A2B0_f39_CLUT_Input_Int);
+}
+
+//B2A0_f39_CLUT_L_Input_Int.push(B2A0_f39_CLUT_L_Input_Int[2000]);
+A2B0_f39_CLUT_Input_Int.push(A2B0_f39_CLUT_Input_Int[2000]+.0001); // +.0001 ? иначе 100 undef
+
+for (var y = 0; y < 2001; y++) {
+	for (var j=0; j<5; j++){
+	B2A0_f39_CLUT_C_Output_Int[j][y] = parseFloat(B2A0_f39_CLUT_C_Output_Int[j][y].toFixed(2));
+	B2A0_f39_CLUT_M_Output_Int[j][y] = parseFloat(B2A0_f39_CLUT_M_Output_Int[j][y].toFixed(2));
+	B2A0_f39_CLUT_Y_Output_Int[j][y] = parseFloat(B2A0_f39_CLUT_Y_Output_Int[j][y].toFixed(2));
+	B2A0_f39_CLUT_K_Output_Int[j][y] = parseFloat(B2A0_f39_CLUT_K_Output_Int[j][y].toFixed(2));
+		A2B0_f39_CLUT_C_Int[j][y] = parseFloat(A2B0_f39_CLUT_C_Int[j][y].toFixed(2));
+		A2B0_f39_CLUT_M_Int[j][y] = parseFloat(A2B0_f39_CLUT_M_Int[j][y].toFixed(2));
+		A2B0_f39_CLUT_Y_Int[j][y] = parseFloat(A2B0_f39_CLUT_Y_Int[j][y].toFixed(2));
+		A2B0_f39_CLUT_K_Int[j][y] = parseFloat(A2B0_f39_CLUT_K_Int[j][y].toFixed(2));
+	}
+}
+
+for (var j=0; j<5; j++){
+B2A0_f39_CLUT_C_Output_Int[j].push(B2A0_f39_CLUT_C_Output_Int[j][2000]);
+B2A0_f39_CLUT_M_Output_Int[j].push(B2A0_f39_CLUT_M_Output_Int[j][2000]);
+B2A0_f39_CLUT_Y_Output_Int[j].push(B2A0_f39_CLUT_Y_Output_Int[j][2000]);
+B2A0_f39_CLUT_K_Output_Int[j].push(B2A0_f39_CLUT_K_Output_Int[j][2000]);
+A2B0_f39_CLUT_C_Int[j].push(A2B0_f39_CLUT_C_Int[j][2000]); 
+A2B0_f39_CLUT_M_Int[j].push(A2B0_f39_CLUT_M_Int[j][2000]);
+A2B0_f39_CLUT_Y_Int[j].push(A2B0_f39_CLUT_Y_Int[j][2000]);
+A2B0_f39_CLUT_K_Int[j].push(A2B0_f39_CLUT_K_Int[j][2000]);
+}
+
+// function cubicsplineChart(массив X, массив Y, массив X-интерп)
+// cubicspline return - массив Y-интерп
+function cubicspline(bG,bH,aM){
+	var H = bG.length;var bo = aM.length;var L=H;var O=new Array(H);var T=new Array(H);var G=new Array(H);var ap=new Array(bo);var bm=new Array(bo);var t=new Array(2);t[0]=new Array(H);t[1]=new Array(H);var next=new Array(2);var F=1,K,C=0,V,ab;var J=0;var l,v,bP;var ac,bF,R,aJ,aS,bk,bb,h,aR,bM;for(var i=0;i<bo;i++){ap[J]=aM[J];J++;}V=0;for(var i=0;i<L;i++){v=parseFloat(bG[V]);bP=parseFloat(bH[V]);V++;if(!isNaN(v)&& !isNaN(bP)){O[i]=v;T[i]=bP;}}K=1;for(var i=0;i<(L-1);i++){t[0][K]=O[K]-O[i];t[1][K]=(T[K]-T[i])/t[0][K];K++;}if(L==2){t[1][0]=t[0][0]=1.0;G[0]=2.0*t[1][1];}else{v=l=t[0][1];t[1][0]=t[0][2];t[0][0]=v+t[0][2];l*=l*t[1][2];G[0]=((v+2.0*t[0][0])*t[1][1]*t[0][2]+l)/t[0][0];}ab=L-1;for(var i=1;i<ab;i++){v= -(t[0][i+1]/t[1][i-1]);G[i]=v*G[i-1]+3.0*(t[0][i]*t[1][i+1]+t[0][i+1]*t[1][i]);t[1][i]=v*t[0][i-1]+2.0*(t[0][i]+t[0][i+1]);}if(L==2){G[1]=t[1][1];}else{if(L==3){G[2]=2.0*t[1][2];t[1][2]=1.0;v= -(1.0/t[1][1]);}else{v=t[0][L-2]+t[0][L-1];l=t[0][L-1]*t[0][L-1]*(T[L-2]-T[L-3]);l/=t[0][L-2];G[L-1]=((t[0][L-1]+2.0*v)*t[1][L-1]*t[0][L-2]+l)/v;v= -(v/t[1][L-2]);t[1][L-1]=t[0][L-2];}t[1][L-1]=v*t[0][L-2]+t[1][L-1];G[L-1]=(v*G[L-2]+G[L-1])/t[1][L-1];}for(var i=L-2;i>=0;i--){G[i]=(G[i]-t[0][i]*G[i+1])/t[1][i];}while(C<J){for(V=C;V<J;V++){if(ap[V]>=O[F])break;}if(V<J){if(F==(L-1))V=J;}ab=V-C;if(ab>0){aR=h=O[F]-O[F-1];next[1]=next[0]=0;bM=0.0;aS=(T[F]-T[F-1])/h;bk=(G[F-1]-aS)/h;bb=(G[F]-aS)/h;ac= -(bk+bk+bb);bF=ac+ac;R=(bk+bb)/h;aJ=R+R+R;for(k=0;k<ab;k++){l=ap[C+k]-O[F-1];bm[C+k]=T[F-1]+l*(G[F-1]+l*(ac+l*R));if(l<bM)next[0]=next[0]+1;if(l>aR)next[1]=next[1]+1;}if((next[0]>0)&&(F!=1)){for(k=C;k<V;k++){if(ap[k]<O[F-1])break;}V=k;for(k=0;k<F;k++){if(ap[V]<O[k])break;}F=(((k-1)>0)?(k-1):0);}C=V;}F++;if(F>=L)break;}
+return bm;
+}  //End of function cubicsplineChart
